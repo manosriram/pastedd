0328197c7bc753c53db7c1de020f7a1573948af6
Mano Sriram <mano.sriram0@gmail.com>
2020-08-26 13:28:50 +0530

    Setup Paste-DB

--- paste-server/src/server.ts
+++ paste-server/src/server.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
 const app = express();
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT;
 
-// Important Middlewares
+// Fundamental Middlewares
 app.use(express.json());
 
+async function startDB() {
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI must be defined");
+    }
+    mongoose.connect(process.env.MONGO_URI!, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+    console.log("Paste-Server MongoDB Connected.");
+}
 
-
+startDB();
 app.listen(PORT, () => console.log(`Paste-Server at ${PORT}`));
+
+export { app };
